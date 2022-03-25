b3c0b4f5cc1a1e14eec0f7070fed10ed145c0ff8
Ahnjihyeon <ajhrose@naver.com>
2022-03-25 16:38:36 +0900

    Home 페이지 수정 및 이벤트 삽입

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,44 @@ import logoWhite from './images/tier_logo_white.png'
 import imgBig from './images/n_logo.png'
 
 const GoHome = () => {
+    
+    const onClickLinkage = () => {
+        console.log("계좌 링크");
+    }
+
+    const onClickWallet = () => {
+        console.log("지갑 연동");
+    }
+
+    const onClickAccount = () => {
+        console.log("계좌 연동");
+    }
+
+    const onClickCard = () => {
+        console.log("카드 연동");
+    }
+
+    const onClickAdd = () => {
+        console.log("결제 수단 추가");
+    }
+
+    const onClickEvent = () => {
+        console.log("이벤트 가기");
+    }
+
+    const onClickHouse = () => {
+        console.log("홈 버튼");
+    }
+
+    const onClickQr = () => {
+        console.log("큐알 코드");
+    }
+
+    const onClickMenu = () => {
+        console.log("메뉴 버튼");
+    }
+
+
     return(
         <div>
             <div className="container">
@@ -20,7 +58,7 @@ const GoHome = () => {
                         <img src={alarmGo} alt="alarm" />
                     </div>
                 </div>
-                <div className="linkage">
+                <div className="linkage" onClick={onClickLinkage}>
                     <img src={imgBig} className="nedlogo" alt="bigN" />
                     <label>
                         <span className="linktypo1">Wallet in my hand</span><br/>
@@ -28,26 +66,51 @@ const GoHome = () => {
                     </label>
                 </div>
                 <div className="yourinfo">
-                    <label className="infotypo1"><span>WALLET</span>
-                    <span>R 0.00</span></label><br/>
-                    <span className="infotypo2">No Wallet registered</span><br/>
-                    
-                    <label className="infotypo1"><span>ACCOUNT</span>
-                    <span>R 0.00</span></label><br/>
-                    <span className="infotypo2">No Account registered</span><br/>
-                    
-                    <label className="infotypo1"><span>CARD</span>
-                    <span>R 0.00</span></label><br/>
-                    <span className="infotypo2">No card registered</span>
-                </div>
-                <div className="addpay">
+
+                    <div className="wallet" onClick={onClickWallet}>
+                        <label className="wallettypo1">
+                            <label><span>WALLET</span>&nbsp;&nbsp;<span className="walletscore">0</span></label>
+                            <span>R 0.00</span>
+                        </label>
+                        <br/>
+                        <span className="wallettypo2">No Wallet registered</span>
+                    </div>
+
+                    <div className="account" onClick={onClickAccount}>
+                        <label className="accounttypo1">
+                            <label><span>ACCOUNT</span>&nbsp;&nbsp;<span className="accountscore">0</span></label>
+                            <span>R 0.00</span>
+                        </label>
+                        <br/>
+                        <span className="accounttypo2">No Account registered</span>
+                    </div>
+
+                    <div className="card" onClick={onClickCard}>
+                        <label className="cardtypo1">
+                            <label><span>CARD</span>&nbsp;&nbsp;<span className="cardscore">0</span></label>
+                            <span>R 0.00</span>
+                        </label>
+                        <br/>
+                        <span className="cardtypo2">No card registered</span>
+                    </div>
+
+                </div>    
+                <div className="addpay" onClick={onClickAdd}>
                     <img src={moreAdd} className="addpayimg" alt="plus"/>
                     <span className="addpaytypo">Add payment method</span>
                 </div>
-                <div className="event">
+                <div className="event" onClick={onClickEvent}>
                     <img src={threeCoin} className="eventimg" alt="coins"/>
+                    <label>
                     <span className="eventtypo1">EVENT</span>
-                    <span className="eventtypo2">Check attendance and get free points! The accumulated points can be used like cash.</span>
+                    <br/><span className="eventtypo2">Check attendance and get free points! The accumulated points can be used like cash.</span>
+                    </label>
+
+                </div>
+                <div className="fixmenu">
+                    <img scr={houseGo} className="houseicon" alt="house" onClick={onClickHouse} />
+                    <img scr={qrGo} className="qricon" alt="qrcode" onClick={onClickQr} />
+                    <img scr={menuGo} className="menuicon" alt="etcicon" onClick={onClickMenu} />
                 </div>
                 
             </div>
